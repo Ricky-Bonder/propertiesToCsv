227c0ab196640509719177f627e00934a6cee8c6
Riccardo Ossola <rickyossola@yahoo.it>
2022-03-01 16:49:16 +0100

    properties to csv works correctly

---
+++ propToCsv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import csv
+
+header = ['', 'english', 'italian', 'deutsch', 'french', 'spanish']
+valueProperty = list()
+
+
+with open('/home/rossola/VSCodeProjects/propertiesToCsv/propertiesFiles/root/root.properties') as f:
+    for entry in f.readlines():
+        if entry.endswith('\n'):
+            entry = entry.rstrip('\n')
+        value = entry.split("=")
+        print("couple key-value: "+value)
+        valueProperty.append(value)
+    print(valueProperty)
+    with open('/home/rossola/VSCodeProjects/propertiesToCsv/properties.csv', 'a+') as csvFile:
+            dw = csv.DictWriter(csvFile, delimiter=',', fieldnames=header)
+            dw.writeheader()
+            writer = csv.writer(csvFile)
+            writer.writerows(valueProperty)
