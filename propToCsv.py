3543a6261ae5c28acf21f4f4458c36b5c6d8cb76
Ricky-Bonder <rickyossola@yahoo.it>
2022-03-02 12:18:35 +0100

    property to Csv conversion creates only one file

--- propToCsv.py
+++ propToCsv.py
@@ -1,21 +1,30 @@
 #!/usr/bin/python
 
 import csv
+import glob
+import os
+import configparser
 
-header = ['', 'english', 'italian', 'deutsch', 'french', 'spanish']
+header = ['', 'english', 'italian', 'deutsch', 'french', 'spanish', 'ofFile']
+files = ['root', 'cagepreparation', 'changepartialscreen', 'changetotalscreen', 'notification', 'reader', 'settings']
 valueProperty = list()
+path = '/home/rossola/VSCodeProjects/propertiesToCsv/propertiesFiles/'
 
-
-with open('/home/rossola/VSCodeProjects/propertiesToCsv/propertiesFiles/root/root.properties') as f:
-    for entry in f.readlines():
-        if entry.endswith('\n'):
-            entry = entry.rstrip('\n')
-        value = entry.split("=")
-        print("couple key-value: ", value)
-        valueProperty.append(value)
-    print(valueProperty)
-    with open('/home/rossola/VSCodeProjects/propertiesToCsv/properties.csv', 'a+') as csvFile:
-            dw = csv.DictWriter(csvFile, delimiter=',', fieldnames=header)
-            dw.writeheader()
-            writer = csv.writer(csvFile)
-            writer.writerows(valueProperty)
+for file in files:
+    seq = (path, file, '.properties')
+    filepath = ''.join(seq)
+    with open(filepath, errors='ignore') as f:
+        for entry in f.readlines():
+            if entry.endswith('\n'):
+                entry = entry.rstrip('\n')
+            value = entry.split("=", 1)
+            print("couple key-value: ", value)
+            valueProperty.append(value)
+        print(valueProperty)
+        with open('/home/rossola/VSCodeProjects/propertiesToCsv/properties.csv', 'a+') as csvFile:
+                dw = csv.DictWriter(csvFile, delimiter=',', fieldnames=header)
+                dw.writeheader()
+                writer = csv.writer(csvFile)
+                writer.writerows(valueProperty)
+                writer.writerow('\n')
+    valueProperty.clear()
